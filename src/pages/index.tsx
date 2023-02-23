04950a1a19c96fa4e7ee6fb2a9756ff061a7f82d
yousaf wazir <umbrellaman612@gmail.com>
2023-02-23 10:39:34 +0000

    changed results font boldness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,9 @@ export default function Page() {
       className={`min-h-screen flex items-center justify-center ${hanken_grotesk.className}`}
     >
       {/* Box container */}
-      <div className="w-[45rem] h-[30rem] rounded-3xl grid grid-cols-2 grid-rows-1">
+      <div className="w-[45rem] h-[29rem] rounded-3xl grid grid-cols-2 grid-rows-1">
         {/* Result box */}
-        <div className="flex flex-col items-center gap-8 py-6 rounded-3xl bg-gradient-to-b from-[#7857FF] to-[#2E2BE9]">
+        <div className="flex flex-col items-center gap-6 py-6 rounded-3xl bg-gradient-to-b from-[#7857FF] to-[#2E2BE9]">
           <p className="text-2xl font-semibold text-[#C8C7FF]">Your Result</p>
           {/* Circle */}
           <div className="w-[12rem] gap-2 h-[12rem] rounded-full flex items-center justify-center flex-col bg-gradient-to-b from-[#4E21CA] to-[#2f2dd2]">
@@ -22,11 +22,11 @@ export default function Page() {
               of 100
             </span>
           </div>
-          <p className="text-3xl font-semibold leading-tight tracking-wide text-white">
+          <p className="text-3xl font-bold leading-tight tracking-wide text-white">
             Great
           </p>
-          <p className="max-w-xs text-base font-medium text-center text-gray-400">
-            You scored higher than 65% of the people who had taken these tests
+          <p className="max-w-xs text-base font-light text-center text-gray-400">
+            You scored higher than 65% of <br /> the people who had taken <br /> these tests
           </p>
         </div>
         {/* Summary box */}
